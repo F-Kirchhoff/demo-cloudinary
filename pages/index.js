b565bfaaaf84c9de4eff3bac8ffe4b657707599c
Felix Kirchhoff <felix.kirchhoff@neuefische.de>
2023-01-24 11:09:42 +0100

    reset

--- pages/index.js
+++ pages/index.js
@@ -2,64 +2,6 @@ import { useState } from "react";
 import Image from "next/image";
 import styled from "styled-components";
 
-export default function Home() {
-  const [images, setImages] = useState([
-    {
-      id: "0",
-      url: "/img1.png",
-      width: 300,
-      height: 300,
-      alt: "first image",
-    },
-  ]);
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-
-    try {
-      const response = await fetch("/api/upload", {
-        method: "post",
-        body: formData,
-      });
-      const data = await response.json();
-      console.log(data);
-
-      setImages((prevImages) => [
-        ...prevImages,
-        {
-          id: data.version_id,
-          url: data.secure_url,
-          width: data.width,
-          height: data.height,
-        },
-      ]);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  return (
-    <AppContainer>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="file" name="file" required />
-        <button type="submit">Upload</button>
-      </form>
-      <ImagesContainer>
-        {images.map(({ id, url, width, height }) => (
-          <li key={id}>
-            <StyledImage
-              src={url}
-              width={width}
-              height={height}
-              alt="uploaded image"
-            />
-          </li>
-        ))}
-      </ImagesContainer>
-    </AppContainer>
-  );
-}
-
 const AppContainer = styled.main`
   display: grid;
   min-height: 100vh;
@@ -83,3 +25,41 @@ const StyledImage = styled(Image)`
   border-radius: 14px;
   overflow: hidden;
 `;
+
+export default function Home() {
+  const [images, setImages] = useState([
+    {
+      id: "0",
+      url: "/img1.png",
+      width: 300,
+      height: 300,
+      alt: "first image",
+    },
+  ]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+  }
+
+  return (
+    <AppContainer>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="file" name="file" required />
+        <button type="submit">Upload</button>
+      </form>
+      <ImagesContainer>
+        {images.map(({ id, url, width, height }) => (
+          <li key={id}>
+            <StyledImage
+              src={url}
+              width={width}
+              height={height}
+              alt="uploaded image"
+            />
+          </li>
+        ))}
+      </ImagesContainer>
+    </AppContainer>
+  );
+}
